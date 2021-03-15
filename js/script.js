27e71a788a7dc19f3314b334f72db1260e01fdc2
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-03-15 14:03:45 +0100

    question 2

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//* Question 1 *//
+
+var outOfStock = true;
+
+if (outOfStock === true) {
+    console.log("Out of stock");
+}
+
+else {
+    console.log("In stock");
+}
+
+//* Question 2 *//
+
+for (var count = 15; count <=25; count++) {
+    if (count == 17 || count == 20){
+        console.log(count);
+    }
+    
+}
