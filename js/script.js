34b072bf6b44e63bac3aaa6984be87b33b6aba65
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-03-16 12:02:17 +0100

    y

--- js/script.js
+++ js/script.js
@@ -18,3 +18,38 @@ for (var count = 15; count <=25; count++) {
     }
     
 }
+
+//* Question 3 *//
+
+var games = [
+	{
+		title: "Grand Theft Auto",
+		rating: 4.48,
+	},
+	{
+		title: "Portal 2",
+		rating: 3.5,
+	},
+	{
+		title: "Team Fortress",
+		rating: null,
+	},
+	{
+		title: "The Witcher",
+		rating: 3.0,
+	},
+	{
+		title: "The Elder Scrolls",
+		rating: 2.9,
+	},
+];
+
+for (var i =0; i < games.length; i++) {
+    var title= games[i].title;
+    var rating= games[i].rating;
+
+    if (rating < 3.5) {
+        console.log(title);
+    }
+
+}
